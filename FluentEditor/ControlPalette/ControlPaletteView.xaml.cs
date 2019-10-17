5afbc90e06dbe8202b009f09e3203bcb9bf48c29
Kiki Saintonge <matrixtunnel@gmail.com>
2019-10-17 13:57:56 -0700

    Corner radii hooked up to customizable values

--- FluentEditor/ControlPalette/ControlPaletteView.xaml.cs
+++ FluentEditor/ControlPalette/ControlPaletteView.xaml.cs
@@ -5,6 +5,9 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Windows;
+using System;
+using System.Threading;
 
 namespace FluentEditor.ControlPalette
 {
@@ -75,6 +78,101 @@ namespace FluentEditor.ControlPalette
             tabButton.Foreground = (SolidColorBrush)App.Current.Resources["ButtonForegroundThemeBrush"];
             tabButton.Opacity = 0.6;
             tabPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void ControlRoundSlider_ValueChanged(object sender, Windows.UI.Xaml.Controls.Primitives.RangeBaseValueChangedEventArgs e)
+        {
+            CornerRadius cornerRadius = (CornerRadius)App.Current.Resources["ControlCornerRadius"];
+            App.Current.Resources["ControlCornerRadius"] = new CornerRadius(ControlRoundSlider.Value);
+
+            RefreshControls();
+        }
+
+        private void ControlRoundSliderOverlay_ValueChanged(object sender, Windows.UI.Xaml.Controls.Primitives.RangeBaseValueChangedEventArgs e)
+        {
+            CornerRadius cornerRadius = (CornerRadius)App.Current.Resources["OverlayCornerRadius"];
+            App.Current.Resources["OverlayCornerRadius"] = new CornerRadius(ControlRoundSliderOverlay.Value);
+        }
+
+        private void RefreshControls()
+        {
+            LightTestContent.RequestedTheme = ElementTheme.Dark;
+            LightTestContent.RequestedTheme = ElementTheme.Light;
+
+            DarkTestContent.RequestedTheme = ElementTheme.Light;
+            DarkTestContent.RequestedTheme = ElementTheme.Dark;
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            textBox.SelectAll();
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            UpdateCorners();
+        }
+
+        private void UpdateCorners()
+        {
+            App.Current.Resources["ControlCornerRadius"] =
+                new CornerRadius(Double.Parse(TopLeftTB.Text), Double.Parse(TopRightTB.Text), 
+                Double.Parse(BottomRightTB.Text), Double.Parse(BottomLeftTB.Text));
+            RefreshControls();
+        }
+
+        private void TextBox_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            if(e.Key == Windows.System.VirtualKey.Enter)
+            {
+                UpdateCorners();
+            }
+        }
+
+        private void ControlBorderThicknessSlider_ValueChanged(object sender, Windows.UI.Xaml.Controls.Primitives.RangeBaseValueChangedEventArgs e)
+        {
+            UpdateBorderThickness(new Thickness(ControlBorderThicknessSlider.Value));
+            RefreshControls();
+        }
+
+        private void UpdateBorderThickness(Thickness value)
+        {
+            App.Current.Resources["ComboBoxBorderThemeThickness"] = value;
+            App.Current.Resources["DatePickerBorderThemeThickness"] = value;
+            App.Current.Resources["TimePickerBorderThemeThickness"] = value;
+            App.Current.Resources["TextControlBorderThemeThickness"] = value;
+            App.Current.Resources["CheckBoxBorderThemeThickness"] = value;
+            App.Current.Resources["ToggleSwitchOuterBorderStrokeThickness"] = value;
+            App.Current.Resources["RadioButtonBorderThemeThickness"] = value;
+        }
+
+        private void TextBoxOverlay_LostFocus(object sender, RoutedEventArgs e)
+        {
+            UpdateOverlayCorners();
+        }
+
+        private void UpdateOverlayCorners()
+        {
+            App.Current.Resources["OverlayCornerRadius"] =
+                new CornerRadius(Double.Parse(TopLeftTBOverlay.Text), Double.Parse(TopRightTBOverlay.Text),
+                Double.Parse(BottomRightTBOverlay.Text), Double.Parse(BottomLeftTBOverlay.Text));
+        }
+
+        private void TextBoxOverlay_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                UpdateOverlayCorners();
+            }
+        }
+
+        private void TextBoxBorder_LostFocus(object sender, RoutedEventArgs e)
+        {
+            UpdateBorderThickness(new Thickness(Double.Parse(LeftBorderTB.Text), Double.Parse(TopBorderTB.Text),
+                Double.Parse(RightBorderTB.Text), Double.Parse(BottomBorderTB.Text)));
+
+            RefreshControls();
         }
     }
 }
